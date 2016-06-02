43ac399aabdbec5bcbc9aa500e4559bde19e65b7
Michael Lancaster <michaell.llancaster@gmail.com>
2016-06-01 23:46:06 -0500

    improve redux setup

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,25 +4,10 @@ require('styles/App.css');
 import React, { Component } from 'react';
 
 class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      status: false
-    }
-  }
-
-  componentDidMount() {
-    this.setState({
-      status: false
-    });
-  }
-
   render() {
-    let status = this.state;
-
     return (
       <div className="index">
-        component did mount? {status}
+        main content
       </div>
     );
   }
---
+++ src/stores/configureStore.js
@@ -0,0 +1,34 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import createLogger from 'redux-logger';
+
+// reducers combined
+import reducers from '../reducers/index';
+
+let logger, store;
+
+export default function configureStore(initialState) {
+  if ( process.env.NODE_ENV !== 'production' ) {
+    logger = createLogger();
+
+    store = createStore(reducers, initialState, applyMiddleware(
+      thunk,
+      logger
+    ));
+
+  } else {
+    store = createStore(reducers, initialState, applyMiddleware(
+      thunk
+    ));
+  }
+
+  if (module.hot) {
+    // Enable Webpack hot module replacement for reducers
+    module.hot.accept('../reducers', () => {
+      const nextReducer = require('../reducers');
+      store.replaceReducer(nextReducer)
+    })
+  }
+
+  return store
+}
